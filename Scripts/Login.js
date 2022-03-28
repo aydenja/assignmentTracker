6d7f189ba6339b6b62bbdc7efc38d24f9347bfc8
Om Shukla <ovshukla@iastate.edu>
2022-03-28 16:37:29 -0500

    fixed bug that didnt check for password length.

--- Scripts/Login.js
+++ Scripts/Login.js
@@ -1,11 +1,18 @@
+/**
+ * Checks to make sure that the email is in the correct format, and checks that the password is atleast 8 characters long.
+ * @returns true if email is in correct format.
+ */
+
 function Validate() {
     var regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
     var isValid = regex.test(document.getElementById("Email").value);
     if (!isValid) {
         //location.reload();
         alert("Enter a valid email address.");
+    } else if(document.getElementById("pwd").value.length < 8) {
+        alert("Enter a valid password.");
     } else {
-        window.open("../assignment-tracker/Home.html"/*, "_self"*/);
+        window.open("../assignment-tracker/Home.html", "_self");
     }
     return isValid;
 }
