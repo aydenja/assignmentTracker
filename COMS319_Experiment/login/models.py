e7dce32eab7b5e2358d562e95707f8b1b3c3d814
Akash Setti <78007732+asetti2002@users.noreply.github.com>
2022-04-17 22:37:19 -0500

    deleted the experiment files

--- COMS319_Experiment/login/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Login(models.Model):
-    user_id = models.IntegerField()
-    username = models.CharField(max_length=64)
-    phash = models.CharField(max_length=64)
-    fname = models.CharField(max_length=64)
-    lname = models.CharField(max_length=64)
